207bf29c00a687465730782aebcd396964e72675
trailsofink <trailsofink@gmail.com>
2025-08-06 15:50:39 -0500

    user and photos validates and private default.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_06_195709) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_06_204814) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pg_catalog.plpgsql"
@@ -222,7 +222,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_06_195709) do
     t.string "avatar_image"
     t.string "bio"
     t.string "website"
-    t.boolean "private"
+    t.boolean "private", default: true
     t.integer "likes_count", default: 0
     t.integer "comment_count", default: 0
     t.datetime "created_at", null: false
