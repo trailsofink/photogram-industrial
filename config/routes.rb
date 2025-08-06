a8920f1393153e9d599cdb1cdbf90b270c8a6a80
trailsofink <trailsofink@gmail.com>
2025-08-06 17:24:25 -0500

    added nav and cdn assets.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  resources :likes
-  resources :follow_requests
-  resources :comments
-  resources :photos
+  # Defines the root path route ("/")
+  root to: "photos#index"
+
   devise_for :users
+
+  resources :comments
+  resources :follow_requests
+  resources :likes
+  resources :photos
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -13,7 +18,4 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  root to: "photos#index"
 end
