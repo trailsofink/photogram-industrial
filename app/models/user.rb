7d8797329398b24e8bd273cc28eea498d7d60f29
trailsofink <trailsofink@gmail.com>
2025-08-07 16:00:05 -0500

    redirecting follow forms.

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,18 +33,29 @@ class User < ApplicationRecord
     :recoverable, :rememberable, :validatable
 
   mount_uploader :avatar_image, ImageUploader
+
   has_many :likes, foreign_key: :fan_id
+  has_many :liked_photos, through: :likes, source: :photo
   has_many :own_photos, foreign_key: "owner_id", class_name: "Photo"
   has_many :comments, foreign_key: "author_id"
+
   has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest"
   has_many :accepted_sent_follow_requests, -> { accepted }, foreign_key: :sender_id, class_name: "FollowRequest"
+
   has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
   has_many :accepted_received_follow_requests, -> { accepted }, foreign_key: :recipient_id, class_name: "FollowRequest"
-  has_many :liked_photos, through: :likes, source: :photo
-  has_many :leaders, through: :sent_follow_requests, source: :recipient
+  has_many :pending_received_follow_requests, -> { pending }, foreign_key: :recipient_id, class_name: "FollowRequest"
+
+  has_many :pending, through: :pending_received_follow_requests, source: :sender
+  has_many :leaders, through: :accepted_sent_follow_requests, source: :recipient
   has_many :followers, through: :accepted_received_follow_requests, source: :sender
+
   has_many :feed, through: :leaders, source: :own_photos
   has_many :discover, -> { distinct }, through: :leaders, source: :liked_photos
 
   validates :username, presence: true, uniqueness: true
+
+  # def only_host_and_port(url)
+  #   url.gsub(%r{^https?://}, "")
+  # end
 end
