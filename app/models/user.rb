a578fece14fe5b3044cd70641f5e11a924acaac0
trailsofink <trailsofink@gmail.com>
2025-08-07 10:43:37 -0500

    fixing user model :leader many to many sent AND accepted requests relation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,7 +41,7 @@ class User < ApplicationRecord
   has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
   has_many :accepted_received_follow_requests, -> { accepted }, foreign_key: :recipient_id, class_name: "FollowRequest"
   has_many :liked_photos, through: :likes, source: :photo
-  has_many :leaders, through: :sent_follow_requests, source: :recipient
+  has_many :leaders, through: :accepted_sent_follow_requests, source: :recipient
   has_many :followers, through: :accepted_received_follow_requests, source: :sender
   has_many :feed, through: :leaders, source: :own_photos
   has_many :discover, -> { distinct }, through: :leaders, source: :liked_photos
